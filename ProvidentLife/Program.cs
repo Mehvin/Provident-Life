eaa18a888bf99c1b532dac2189496ae30f6ea44d
Melvin <melvin_omg@ymail.com>
2018-02-02 15:04:46 +0800

    Polished main program

--- ProvidentLife/Program.cs
+++ ProvidentLife/Program.cs
@@ -11,51 +11,159 @@ namespace ProvidentLife
         static void Main(string[] args)
         {
             string user;
-            int option = 0;
-            bool programrun = true;
-
+             
             Console.WriteLine("----Provident Life System----");
             Console.Write("Username: ");
             user = Console.ReadLine();
+            Console.WriteLine("");
+
+            // check user type
+            if (user == "customer") // temporary way of checking of user
+            {
+                customerSystem(user);
+            }
+            else if (user == "staff") // temporary way of checking of user
+            {
+                staffSystem(user);
+            }
+        }
+
+        static void customerSystem(string user)
+        {
+            Console.WriteLine("Welcome back Customer, " + user + "\n");
+            bool programrun = true;
 
             while (programrun)
             {
-                DisplayMenu();
+                Console.WriteLine("-----Customer Tasks-----");
+                Console.WriteLine("[1] View Policy");
+                Console.WriteLine("[0] Exit");
+
                 Console.Write("Enter option: ");
-                option = Convert.ToInt32(Console.ReadLine());
+                int option = Convert.ToInt32(Console.ReadLine());
 
-                if (option == 1)
+                if (option == 1) //View policy
                 {
-                    //Funtion Create new policy
+                    //view own policies
+                    //customerViewPolicy();
                 }
-                else if (option == 2)
+                else if (option == 0) //Exit
                 {
-                    //Function View All Policy
-
-                    //Selects policy number
-                    //display policy menu
-                    DisplayPolicyMenu();
-                }
-                else if (option == 0)
-                {
-                    //exit
                     programrun = false;
                 }
-            }       
+            }
         }
-            
-        static void DisplayMenu()
+
+        static void customerViewPolicy()
         {
-            Console.WriteLine("\nProvident Life System");
-            Console.WriteLine("[1] Create New Policy");
-            Console.WriteLine("[2] View All Policy"); // if user = admin show all , if user != admin show their own policies only
-            Console.WriteLine("[0] Exit");
+
+        }
+
+        static void staffSystem(string user)
+        {
+            Console.WriteLine("Welcome back Staff, " + user + "\n");
+            bool programrun = true;
+
+            while (programrun)
+            {
+                Console.WriteLine("-----Staff Tasks-----");
+                Console.WriteLine("[1] Create New Policy");
+                Console.WriteLine("[2] View Policy"); // if user = admin show all , if user != admin show their own policies only
+                Console.WriteLine("[0] Exit");
+
+                Console.Write("Enter option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1) //Create new policy
+                {
+                    createPolicy();
+                }
+                else if (option == 2) //View policy
+                {
+                    //Selects policy number
+                    //display policy menu
+                    //staffViewPolicy();
+                }
+                else if (option == 0) //Exit
+                {
+                    programrun = false;
+                }
+            }
+        }
+
+        static void createPolicy()
+        {
+            //Loop to get all clients and display it
+
+            Console.Write("Select a client: ");
+            int clientNo = Convert.ToInt32(Console.ReadLine());
+
+            //Loop to get list of policy types and display it
+
+            Console.Write("Select a policy type: ");
+            int policyType = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Select premium type(1-One Time| 2-Periodic): ");
+            int premiumType = Convert.ToInt32(Console.ReadLine());
+
+            if (premiumType == 2)
+            {
+                Console.Write("Number of days per period: ");
+                int noOfDays = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Enter total amount: $");
+            int totalAmt = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter maturity date: ");
+            DateTime maturityDate = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Enter no. of additional riders: ");
+            int addRiders = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter terms and conditions: ");
+            string TnC = Console.ReadLine();
+
+            //Display details of insurance policy
+
+            Console.Write("Confirm to create policy? (1-Confirm | 2-Cancel): ");
+            int confirm = Convert.ToInt32(Console.ReadLine());
+
+            if (confirm == 1)
+            {
+                // create new policy
+                Console.WriteLine("Policy created!");
+            }
+        }
+
+        static void staffViewPolicy()
+        {
+            //check if user is admin
+
+            //if admin
+            //display ALL policies
+
+            //if not admin
+            //display polcies that belongs to him
+
+            //add transaction to edit policy (sterling's)
+            //add transaction to generate alerts (bryan's)
         }
 
         static void DisplayPolicyMenu()
         {
             Console.WriteLine("\n1. Generate Alerts");
             Console.WriteLine("2. Edit Policy");
+        }
+
+        static void editPolicy()
+        {
+
+        }
+
+        static void generateAlerts()
+        {
+
         }
     }
 }
